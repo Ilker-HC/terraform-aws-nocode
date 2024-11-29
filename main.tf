3cf88f571e1188e77b228c7bb3519617492fd1b2
Ilker-HC <ilker.vandenberg@hashicorp.com>
2024-11-29 16:27:43 +0100

    Update main.tf
    ingres to port increase by 1

--- main.tf
+++ main.tf
@@ -38,14 +38,14 @@ resource "aws_security_group" "rds" {
 
   ingress {
     from_port   = 5432
-    to_port     = 5432
+    to_port     = 5433
     protocol    = "tcp"
     cidr_blocks = ["192.80.0.0/16"]
   }
 
   egress {
     from_port   = 5432
-    to_port     = 5432
+    to_port     = 5433
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
